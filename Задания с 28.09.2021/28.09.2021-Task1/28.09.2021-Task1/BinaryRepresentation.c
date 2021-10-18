a01418343a72f49c3b392844d0886f7e349606ca
RuslanBeresnev <rusber03102003@gmail.com>
2021-10-18 15:36:55 +0300

    Удалена функция reverseArray(), удалена функция из закомментированной части кода, порядок итераций в цикле в функции bitwiseAddition() изменён на противоположный, изменён механизм перевода из двоичного кода в десятичную запись в функции convertToDecimal()

--- Задания с 28.09.2021/28.09.2021-Task1/28.09.2021-Task1/BinaryRepresentation.c
+++ Задания с 28.09.2021/28.09.2021-Task1/28.09.2021-Task1/BinaryRepresentation.c
@@ -3,90 +3,28 @@
 #include <locale.h>
 #include <stdbool.h>
 #include <math.h>
+
 #define INT_BITS_COUNT sizeof(int) * 8
 
-void copyNumbersArray(int originArray[], int length, int copiedArray[])
-{
-    for (int i = 0; i < length; i++)
-    {
-        copiedArray[i] = originArray[i];
-    }
-}
+#pragma warning (disable: 4774 5045 4996)
 
-void reverseArray(int array[], int length)
+void bitwiseAddition(int firstNumber[], int secondNumber[], int resultNumber[])
 {
-    for (int i = 0; i < length; i++)
-    {
-        const int bufferElement = array[length - 1];
-        for (int j = length - 2; j >= i; j--)
-        {
-            array[j + 1] = array[j];
-        }
-        array[i] = bufferElement;
-    }
-}
-
-void bitwiseAddition(int firstNumber[], int firstLength, int secondNumber[], int secondLength, int resultNumber[])
-{
-    reverseArray(firstNumber, firstLength);
-    reverseArray(secondNumber, secondLength);
     int currentBitSum = 0;
-    int position = 0;
-    const int maxLength = (firstLength < secondLength) ? secondLength : firstLength;
-    while (position < INT_BITS_COUNT && (currentBitSum > 0 || position < maxLength))
+    int position = INT_BITS_COUNT - 1;
+    while (position >= 0)
     {
-        if (position < firstLength)
-        {
-            currentBitSum += (firstNumber[position] == 1) ? 1 : 0;
-        }
-        if (position < secondLength)
-        {
-            currentBitSum += (secondNumber[position] == 1) ? 1 : 0;
-        }
+        currentBitSum += (firstNumber[position] == 1) ? 1 : 0;
+        currentBitSum += (secondNumber[position] == 1) ? 1 : 0;
         resultNumber[position] = currentBitSum % 2;
         currentBitSum /= 2;
-        position++;
+        position--;
     }
-    reverseArray(resultNumber, position);
 }
-
-//void convertToBinary(int number, int binaryArray[])
-//{
-//    bool negativeNumber = false;
-//    if (number < 0)
-//    {
-//        negativeNumber = true;
-//        number = -number;
-//    }
-//    int length = 0;
-//    while (number > 0)
-//    {
-//        binaryArray[length] = number % 2;
-//        number /= 2;
-//        length++;
-//    }
-//    for (int i = length; i < INT_BITS_COUNT; i++)
-//    {
-//        binaryArray[i] = 0;
-//    }
-//    reverseArray(binaryArray, INT_BITS_COUNT);
-//    if (negativeNumber)
-//    {
-//        binaryArray[0] = 1;
-//        for (int i = 1; i < INT_BITS_COUNT; i++)
-//        {
-//            binaryArray[i] = (binaryArray[i] == 1) ? 0 : 1;
-//        }
-//        int unit[1] = { 1 };
-//        int binaryArrayCopy[INT_BITS_COUNT] = { 0 };
-//        copyNumbersArray(binaryArray, INT_BITS_COUNT, binaryArrayCopy);
-//        bitwiseAddition(binaryArrayCopy, INT_BITS_COUNT, unit, 1, binaryArray);
-//    }
-//}
 
 void convertToBinary(int number, int binaryArray[])
 {
-    int bit = 0b00000000000000000000000000000001;
+    int bit = 1;
     for (int i = INT_BITS_COUNT - 1; i >= 0; i--)
     {
         binaryArray[i] = (number & bit) ? 1 : 0;
@@ -97,9 +35,11 @@ void convertToBinary(int number, int binaryArray[])
 int convertToDecimal(int array[], int length)
 {
     int decimalNumber = 0;
-    for (int power = length - 1; power >= 0; power--)
+    int twoInNaturalPower = 1;
+    for (int i = 0; i < length; i++)
     {
-        decimalNumber += (int)pow(2, power) * array[length - power - 1];
+        decimalNumber += twoInNaturalPower * array[length - i - 1];
+        twoInNaturalPower *= 2;
     }
     return decimalNumber;
 }
@@ -108,19 +48,12 @@ void printBinaryNumber(int binaryArray[], int length)
 {
     for (int i = 0; i < length; i++)
     {
-        if (i % 8 == 7)
-        {
-            printf("%d ", binaryArray[i]);
-        }
-        else
-        {
-            printf("%d", binaryArray[i]);
-        }
+        printf(i % 8 == 7 ? "%d " : "%d", binaryArray[i]);
     }
     printf("\n");
 }
 
-void printDecimalNumberInBinary(number)
+void printDecimalNumberInBinary(int number)
 {
     int binaryArray[INT_BITS_COUNT] = { 0 };
     convertToBinary(number, binaryArray);
@@ -134,16 +67,17 @@ bool testModule(int firstNumber, int secondNumber, int supposedSum)
     convertToBinary(firstNumber, firstNumberInBinary);
     convertToBinary(secondNumber, secondNumberInBinary);
     int sumOfTwonumbers[INT_BITS_COUNT] = { 0 };
-    bitwiseAddition(firstNumberInBinary, INT_BITS_COUNT, secondNumberInBinary, INT_BITS_COUNT, sumOfTwonumbers);
+    bitwiseAddition(firstNumberInBinary, secondNumberInBinary, sumOfTwonumbers);
     return convertToDecimal(sumOfTwonumbers, 32) == supposedSum;
 }
 
-bool generalTest()
+bool generalTest(void)
 {
-    return testModule(5, 8, 13) && testModule(-5, -8, -13) && testModule(8, -9, -1) && testModule(0, 0, 0) && testModule(2147483647, 5, -2147483644) && testModule(-2147483647, -2, 2147483647) && testModule(2147483647, 2147483647, -2);
+    return testModule(5, 8, 13) && testModule(-5, -8, -13) && testModule(8, -9, -1) && testModule(0, 0, 0) && 
+        testModule(2147483647, 5, -2147483644) && testModule(-2147483647, -2, 2147483647) && testModule(2147483647, 2147483647, -2);
 }
 
-int main()
+int main(void)
 {
     if (!generalTest())
     {
@@ -179,7 +113,7 @@ int main()
     convertToBinary(firstNumber, firstNumberInBinary);
     convertToBinary(secondNumber, secondNumberInBinary);
     int sumOfTwonumbers[INT_BITS_COUNT] = { 0 };
-    bitwiseAddition(firstNumberInBinary, INT_BITS_COUNT, secondNumberInBinary, INT_BITS_COUNT, sumOfTwonumbers);
+    bitwiseAddition(firstNumberInBinary, secondNumberInBinary, sumOfTwonumbers);
     printf("Сумма двух чисел в двоичном представлении: ");
     printBinaryNumber(sumOfTwonumbers, INT_BITS_COUNT);
     printf("Сумма двух чисел в десятичном представлении: ");
