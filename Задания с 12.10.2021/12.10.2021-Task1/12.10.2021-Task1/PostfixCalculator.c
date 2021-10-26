819ee29a044325e70e09549bbab2d10b5f09f94b
RuslanBeresnev <rusber03102003@gmail.com>
2021-10-26 21:40:43 +0300

    Тесты для функции postfixCalculator() внесены в main(), тип чисел изменён с float на int

--- Задания с 12.10.2021/12.10.2021-Task1/12.10.2021-Task1/PostfixCalculator.c
+++ Задания с 12.10.2021/12.10.2021-Task1/12.10.2021-Task1/PostfixCalculator.c
@@ -1,17 +1,16 @@
 ﻿#pragma warning (disable: 4996)
 
 #include "../../Stack/Stack/Stack.h"
-#include "PostfixCalculatorTests.h"
 
 #include <stdio.h>
 #include <locale.h>
 
-float postfixCalculator(char sequence[])
+int postfixCalculator(char sequence[])
 {
     StackElement* head = NULL;
     int position = 0;
-    float firstNumber = 0;
-    float secondNumber = 0;
+    int firstNumber = 0;
+    int secondNumber = 0;
     while (sequence[position] != '\n')
     {
         if (sequence[position] == '+' || sequence[position] == '-' || sequence[position] == '*' || sequence[position] == '/')
@@ -34,7 +33,7 @@ float postfixCalculator(char sequence[])
             push(&head, secondNumber * firstNumber);
             break;
         case '/':
-            push(&head, (float)firstNumber / secondNumber);
+            push(&head, firstNumber / secondNumber);
             break;
         default:
             push(&head, sequence[position] - '0');
@@ -43,6 +42,21 @@ float postfixCalculator(char sequence[])
         position++;
     }
     return pop(&head);
+}
+
+bool standardSequenceTestPassed(void)
+{
+    return postfixCalculator("5 5 3 2 * + - 3 /\n") == -2;
+}
+
+bool emptySequenceTestPassed(void)
+{
+    return postfixCalculator("\n") == 0;
+}
+
+bool generalTestPassed(void)
+{
+    return standardSequenceTestPassed() && emptySequenceTestPassed();
 }
 
 int main(void)
@@ -56,7 +70,7 @@ int main(void)
     printf("Введите последовательность арифметических операций с помощью постфиксной записи:\n");
     char sequence[100] = { 0 };
     fgets(sequence, 100, stdin);
-    const float result = postfixCalculator(sequence);
-    printf("Полученный результат: %f", result);
+    const int result = postfixCalculator(sequence);
+    printf("Полученный результат: %d", result);
     printf("\n");
 }
