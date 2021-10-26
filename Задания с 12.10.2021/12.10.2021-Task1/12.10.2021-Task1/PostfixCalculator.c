d507ba86da580ed6771a0b083cfd62c55a75ac9f
RuslanBeresnev <rusber03102003@gmail.com>
2021-10-26 19:23:06 +0300

    Initial commit (пока что с нерабочими тестами и предупреждениями)

---
+++ Задания с 12.10.2021/12.10.2021-Task1/12.10.2021-Task1/PostfixCalculator.c
@@ -0,0 +1,62 @@
+﻿#pragma warning (disable: 4996)
+
+#include "../../Stack/Stack/Stack.h"
+#include "PostfixCalculatorTests.h"
+
+#include <stdio.h>
+#include <locale.h>
+
+float postfixCalculator(char sequence[])
+{
+    StackElement* head = NULL;
+    int position = 0;
+    float firstNumber = 0;
+    float secondNumber = 0;
+    while (sequence[position] != '\n')
+    {
+        if (sequence[position] == '+' || sequence[position] == '-' || sequence[position] == '*' || sequence[position] == '/')
+        {
+            secondNumber = pop(&head);
+            firstNumber = pop(&head);
+        }
+        switch (sequence[position])
+        {
+        case ' ':
+            position++;
+            continue;
+        case '+':
+            push(&head, firstNumber + secondNumber);
+            break;
+        case '-':
+            push(&head, firstNumber - secondNumber);
+            break;
+        case '*':
+            push(&head, secondNumber * firstNumber);
+            break;
+        case '/':
+            push(&head, (float)firstNumber / secondNumber);
+            break;
+        default:
+            push(&head, sequence[position] - '0');
+            break;
+        }
+        position++;
+    }
+    return pop(&head);
+}
+
+int main(void)
+{
+    if (!generalTestPassed())
+    {
+        printf("Tests Failed ...\n");
+        return 1;
+    }
+    setlocale(LC_ALL, "Russian");
+    printf("Введите последовательность арифметических операций с помощью постфиксной записи:\n");
+    char sequence[100] = { 0 };
+    fgets(sequence, 100, stdin);
+    const float result = postfixCalculator(sequence);
+    printf("Полученный результат: %f", result);
+    printf("\n");
+}
---
+++ Задания с 12.10.2021/12.10.2021-Task1/12.10.2021-Task1/PostfixCalculatorTests.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdbool.h>
+
+// Проверка работы функции на стандартной последовательности
+bool standardSequenceTestPassed(void);
+
+// Проверка работы функции на пустой последовательности
+bool emptySequenceTestPassed(void);
+
+// Проверка на прохождение всех тестов
+bool generalTestPassed(void);
