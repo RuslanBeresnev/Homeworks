e258cf334477165d12caf29aa9f5a52c8a93d29c
RuslanBeresnev <rusber03102003@gmail.com>
2021-12-13 21:50:05 +0300

    Ещё изменения

--- Задания с 09.11.2021/09.11.2021-Task2/Building.bat
+++ Задания с 09.11.2021/09.11.2021-Task2/Building.bat
@@ -3,9 +3,10 @@
 cd..
 cd..
 git clean -dfx
-for /r cd. %%I in (*.sln) do (
-if not %%I=="C:\\Users\User\source\repos\Homeworks\Задания с 07.09.2021\ForUniversity.sln" MSBuild %%I
+cd..
+for /r "Homeworks" %%I in (*.sln) do (
+if not %%I=="C:\\Users\User\source\repos\Homeworks\Задания с 07.09.2021\ForUniversity.sln" MSBuild "%%I"
 )
-for /r "..\..\..\Homeworks" %%I in (*.exe) do %%I 1
+for /r "Homeworks" %%I in (*.exe) do %%I 1
 
 @pause
