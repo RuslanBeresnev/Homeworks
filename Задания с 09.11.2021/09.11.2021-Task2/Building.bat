380cdf2bfd08b22a41b03aabd02141fb98221082
RuslanBeresnev <rusber03102003@gmail.com>
2021-12-14 20:51:13 +0300

    Все ошибки исправлены, теперь сборка работает !!!

--- Задания с 09.11.2021/09.11.2021-Task2/Building.bat
+++ Задания с 09.11.2021/09.11.2021-Task2/Building.bat
@@ -2,7 +2,5 @@
 cd..
 cd..
 git clean -dfx
-for /r %%I in (*.sln) do (
-if not "%%I"=="..\..\Задания с 07.09.2021\ForUniversity.sln" MSBuild "%%I"
-)
-for /r %%I in (*.exe) do %%I 1
+for /r %%I in (*.sln) do MSBuild "%%I"
+for /r %%I in (*.exe) do "%%I" 1
